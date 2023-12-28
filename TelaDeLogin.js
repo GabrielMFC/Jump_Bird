36e3725c266e4f3c1b03ab163f3edabbde428ebc
Gabriel Matias <ggezcom991gh@gmail.com>
2023-12-28 17:22:40 -0300

    Resolvendo o erro da tela de login

--- TelaDeLogin.js
+++ TelaDeLogin.js
@@ -11,9 +11,6 @@ setInterval(mudarspriteprabaixo, 1200)
 /* Fim do Tópico Animação do passáro na tela de login */
 
 /* Inicio do tópico entrar no jogo */
-let botaodeentrar = document.getElementById("botaodeentrar").addEventListener("click", function entrarNoJogo() {
-    document.getElementById("link").href = "jumpbird.html"
-})
 let botaodesair = document.getElementById("botaodesair").addEventListener("click", function naoSairDoJogo() {
     alert("Eu sei que você não quer sair do jogo, vou te dar outra chance")
 })
